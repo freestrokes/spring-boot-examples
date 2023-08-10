f7b2b1f73ff26356e3c9544ea39889766d334276
freestrokes <freestrokes.hk@gmail.com>
2023-08-10 16:29:28 +0900

    [main] feat: SpringBootExamplesApplicationTests.java 수정

--- src/test/java/com/freestrokes/SpringBootExamplesApplicationTests.java
+++ src/test/java/com/freestrokes/SpringBootExamplesApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-public class SpringBootOAuth2ApplicationTests {
+public class SpringBootExamplesApplicationTests {
 
 	@Test
 	public void contextLoads() {

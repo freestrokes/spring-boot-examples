1066a8b3879d74188369dea2f709a421c4abada3
freestrokes <freestrokes.hk@gmail.com>
2023-08-13 22:27:45 +0900

    [main] feat: HealthCheckController.java 수정

--- src/main/java/com/freestrokes/controller/HealthCheckController.java
+++ src/main/java/com/freestrokes/controller/HealthCheckController.java
@@ -1,29 +1,23 @@
 package com.freestrokes.controller;
 
-import com.freestrokes.aop.LogExecutionTime;
 import com.freestrokes.constants.PathConstants;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// TODO: @RequestMapping
-// 해당 컨트롤러 하위 전체 메서드에 공통 path 설정이 필요한 경우 class 상단에 어노테이션 사용.
-// @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 @RestController
-public class HealthController {
+public class HealthCheckController {
 
-    /**
-     * 게시글 목록 조회
-     *
-     * @return
-     * @throws Exception
-     */
     @GetMapping(path = PathConstants.HEALTH_CHECK, produces = "application/json")
-    @LogExecutionTime
-    public ResponseEntity<?> getBoards() throws Exception {
+    @Operation(
+        summary = "헬스 체크",
+        description = "헬스 체크를 한다."
+    )
+    public ResponseEntity<?> healthCheck() throws Exception {
         return new ResponseEntity<>("{}", HttpStatus.OK);
     }
 

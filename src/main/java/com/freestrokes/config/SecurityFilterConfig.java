91daafeac166cd5aa1b746358aefbb8370a4a1dc
freestrokes <freestrokes.hk@gmail.com>
2024-01-30 09:31:24 +0900

    feat: SecurityFilterConfig 추가

---
+++ src/main/java/com/freestrokes/config/SecurityFilterConfig.java
@@ -0,0 +1,26 @@
+package com.freestrokes.config;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class SecurityFilterConfig extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        FilterChain filterChain
+    ) throws ServletException, IOException {
+        if (request.getMethod().equals("TRACE")) {
+            response.getWriter().println("TRACE method is not allowed");
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+}

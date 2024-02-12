941d95723b49da46a7608a82b6ec65b10f2dcf41
freestrokes <freestrokes.hk@gmail.com>
2024-02-12 18:42:21 +0900

    feat: board repository 수정

--- src/main/java/com/freestrokes/repository/board/BoardRepository.java
+++ src/main/java/com/freestrokes/repository/board/BoardRepository.java
@@ -1,14 +1,25 @@
 package com.freestrokes.repository.board;
 
 import com.freestrokes.domain.board.BoardEntity;
+ //import com.freestrokes.vo.BoardVo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BoardRepository extends JpaRepository<BoardEntity, String> {
 
+    // TODO
+    // VO 인터페이스는 Page 객체의 제네릭으로 사용할 수 없는데
+    // 이 부분에 대해 찾아보기
     Page<BoardEntity> findAll(Pageable pageable);
+
+    // TODO
+//    @Query("SELECT be.boardId FROM BoardEntity be")
+//    List<BoardVo> getBoardList();
 
 }

b784b52a134d026a9cd8d3b94aa740c45bcd77d4
freestrokes <freestrokes.hk@gmail.com>
2024-02-14 11:13:16 +0900

    feat: webclienttests 추가

---
+++ src/test/java/com/freestrokes/WebClientTests.java
@@ -0,0 +1,39 @@
+package com.freestrokes;
+
+import com.ahnlab.asd.presentation.external.api.common.constant.PathConstant;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@ActiveProfiles("local")
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@RequiredArgsConstructor
+class WebClientTests {
+  
+  @Autowired
+  private WebClient webClient;
+  
+  public WebClient request() {
+    return webClient.mutate().baseUrl(
+      "http://localhost:8080"
+    ).build();
+  }
+  
+  @Test
+  public void contextLoads() {
+    request()
+      .get()
+      .uri(PathConstant.HEALTH_CHECK)
+      .retrieve()
+      .bodyToMono(Void.class)
+      .block();
+  }
+  
+}
+ 
